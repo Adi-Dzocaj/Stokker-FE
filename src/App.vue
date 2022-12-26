506cc6718821e4afd2259d8433f6534b5ec474f3
Adi <adidzocaj@gmail.com>
2022-12-26 03:16:49 +0100

    Made a basic footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from "./components/skeleton/Navbar.vue";
+import FooterBar from "./components/skeleton/FooterBar.vue";
 import "./globalStyles/main.css";
 </script>
 
 <template>
   <Navbar />
+  <FooterBar />
   <router-view></router-view>
 </template>
 
---
+++ src/components/skeleton/FooterBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer-container">
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+@media (min-width: 768px) {
+  .footer-container {
+    padding: 10px;
+    display: flex;
+    height: 20vh;
+    width: 100%;
+    background-color: #344d67;
+    position: absolute;
+    bottom: 0;
+  }
+
+  .footer-container div:nth-child(1) {
+    width: 70%;
+    height: 100%;
+    border-right: 1px solid #ffe1a1;
+  }
+}
+</style>
