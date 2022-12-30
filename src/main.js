13ffb94b88caf99f69d30451a9ad68e70e2ec517
Adi <adidzocaj@gmail.com>
2022-12-30 04:51:01 +0100

    Basic email/password registration set up - ( minimum viable product ) - there exists room for optimization.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+
 import { createPinia } from 'pinia';
+
 import router from '/src/router/index.js';
+
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faUserTie,  } from '@fortawesome/free-solid-svg-icons';
 import { faGoogle, faGithub, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faUserTie, faGoogle, faGithub, faFacebook);
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
+};
+
+initializeApp(firebaseConfig);
+
 
 createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).use(createPinia()).mount('#app');
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -36,7 +36,30 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const router = useRouter();
+const email = ref("");
+const password = ref("");
+
+const register = async () => {
+  try {
+    await createUserWithEmailAndPassword(
+      getAuth(),
+      email.value,
+      password.value
+    );
+    console.log("Registered");
+    router.push("/");
+  } catch (error) {
+    console.log(error.code);
+    alert(error.message);
+  }
+};
+</script>
 
 <style scoped>
 .register-container {
