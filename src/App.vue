e100f320e4566960dac05128a692725078cf704f
Adi <adidzocaj@gmail.com>
2022-12-31 04:28:10 +0100

    Implemented the loading of an user onmount and the conditional setting of user state with Pinia action.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,16 @@
 import Navbar from "./components/skeleton/Navbar.vue";
 import FooterBar from "./components/skeleton/FooterBar.vue";
 import "./globalStyles/main.css";
+import { onMounted } from "vue";
+
+import { useUserStore } from "../src/store/userStore/";
+
+const userStore = useUserStore();
+
+onMounted(() => {
+  console.log("mounting..");
+  userStore.loadUser();
+});
 </script>
 
 <template>
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,12 +1,30 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { defineStore } from 'pinia';
+import { getAuth } from 'firebase/auth';
 
 export const useUserStore = defineStore('userStore', {
   state: () => {
     return {
-      
+      user: null,
+      loading: false,
     };
   },
   actions: {
-    
+    loadUser() {
+        let auth = getAuth()
+        this.loading = true;
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                console.log('User has signed in')
+                this.user = user
+                console.log(this.user)
+                this.loading = false
+            } else {
+                console.log('User isnt logged in / hasnt been created')
+                console.log(this.user)
+                this.loading = false
+            }
+        })
+    }
   },
 });
